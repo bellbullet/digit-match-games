1ffdb628df0af34b3e3b7ee41ca92bf35e813c09
bellbullet <keiya.kys@gmail.com>
2026-05-15 00:03:59 +0900

    Simplify App component to return a greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "#111",
-        color: "white",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        fontFamily: "sans-serif"
-      }}
-    >
-      <h1>App Test Success</h1>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
