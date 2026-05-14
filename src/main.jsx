f27873e732eab2fa5d2f1d5baef291561341c3ad
bellbullet <keiya.kys@gmail.com>
2026-05-14 23:51:56 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "black",
-        color: "white",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column"
-      }}
-    >
-      <h1>Digit Match Game</h1>
-      <button>Start</button>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
