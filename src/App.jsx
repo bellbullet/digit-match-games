becc0ddb9591fa029638aab284f0ac4b59fa4e14
bellbullet <keiya.kys@gmail.com>
2026-05-14 23:00:37 +0900

    Change text color from red to white

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
       style={{
         minHeight: "100vh",
         background: "#111",
-        color: "red",
+        color: "white",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
