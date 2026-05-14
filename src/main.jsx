e4c03c60ccbd4242ce3c41d64a17306e16e847d9
bellbullet <keiya.kys@gmail.com>
2026-05-15 00:03:44 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-export default function App() {
-  return <h1>hello</h1>;
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
